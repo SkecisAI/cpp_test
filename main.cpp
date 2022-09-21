29c13db9319b99e2b7728093b9c5f28500fc5e68
SkecisAI <skecis@qq.com>
2022-09-21 22:00:32 +0800

    test vscode

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+struct student
+{
+    int age;
+    std::string name;
+};
+
+
+int main(void)
+{
+    student s = {1, "jack"};
+    int old_v = s.age;
+    s.age = 12;
+    if (s.age != old_v)
+    {
+        std::cout << "age changed." << std::endl;
+    }
+}
